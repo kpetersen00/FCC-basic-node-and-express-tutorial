088f1d4c781b3183d4018f76f38f71e28c36980e
Kyle <petersen_kyle2000@yahoo.com>
2022-01-07 18:57:02 -0800

    using env file

--- myApp.js
+++ myApp.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
-const bGround = require('fcc-express-bground');
 require('dotenv').config();
 console.log('Hello World');
 // app.get('/', (req, res) => res.send('Hello Express'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 app.get('/json', (req, res) => {
-  let response = { message: 'hello json' };
+  let response = { message: 'Hello json' };
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     response.message = response.message.toUpperCase();
   }
