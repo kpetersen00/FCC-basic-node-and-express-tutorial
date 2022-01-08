3d946cda1fa667a854137eac0665f7ca7f9f43c3
Kyle <petersen_kyle2000@yahoo.com>
2022-01-07 19:46:32 -0800

    query params

--- myApp.js
+++ myApp.js
@@ -31,5 +31,14 @@ app.get(
     res.send({ time: req.time });
   }
 );
+
 app.get('/:word/echo', (req, res) => res.json({ echo: req.params.word }));
+
+app.get('/name', (req, res) => {
+  const { first: firstName, last: lastName } = req.query;
+  res.json({
+    name: `${firstName} ${lastName}`,
+  });
+});
+
 module.exports = app;
