cb768a662db606a0e5ff22440bbdb3d3fc0b4047
Kyle <petersen_kyle2000@yahoo.com>
2022-01-07 18:21:08 -0800

    using env file

--- myApp.js
+++ myApp.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
-
+require('dotenv').config();
 console.log('Hello World');
 // app.get('/', (req, res) => res.send('Hello Express'));
-// app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
-app.get('/json', (req, res) => res.json({ message: 'Hello json' }));
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
+app.get('/json', (req, res) => {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json({ message: 'HELLO JSON' });
+  } else {
+    res.json({ message: 'hello json' });
+  }
+});
 app.use('/public', express.static(__dirname + '/public'));
 module.exports = app;
