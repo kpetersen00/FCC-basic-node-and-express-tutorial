85e14a9db26133dc19012dae2233e0c28e037f9c
Kyle <petersen_kyle2000@yahoo.com>
2022-01-07 17:58:19 -0800

    added json

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ var app = express();
 
 console.log('Hello World');
 // app.get('/', (req, res) => res.send('Hello Express'));
-app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
+// app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
+app.get('/json', (req, res) => res.json({ message: 'Hello json' }));
 app.use('/public', express.static(__dirname + '/public'));
 module.exports = app;
