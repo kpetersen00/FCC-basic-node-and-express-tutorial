5b5c9ef4fb25cf9e528ef2a15d801c36a41bd3ec
Kyle <petersen_kyle2000@yahoo.com>
2022-01-07 19:09:21 -0800

    using mware

--- myApp.js
+++ myApp.js
@@ -3,7 +3,14 @@ var app = express();
 require('dotenv').config();
 console.log('Hello World');
 // app.get('/', (req, res) => res.send('Hello Express'));
+
+app.use((req, res, next) => {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  next();
+});
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
+
 app.get('/json', (req, res) => {
   let response = { message: 'Hello json' };
   if (process.env.MESSAGE_STYLE === 'uppercase') {
@@ -11,5 +18,7 @@ app.get('/json', (req, res) => {
   }
   res.json(response);
 });
+
 app.use('/public', express.static(__dirname + '/public'));
+
 module.exports = app;
