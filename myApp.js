4ffc07d8e1eaa187779faa0a7a07faf4cbdeb75b
Kyle <petersen_kyle2000@yahoo.com>
2022-01-07 16:57:02 -0800

    added app.use

--- myApp.js
+++ myApp.js
@@ -4,4 +4,5 @@ var app = express();
 console.log('Hello World');
 // app.get('/', (req, res) => res.send('Hello Express'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
+app.use('/public', express.static(__dirname + '/public'));
 module.exports = app;
