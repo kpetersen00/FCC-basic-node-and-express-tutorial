335cff83019668dbf1260b1f141c173fae4ec18f
Kyle <petersen_kyle2000@yahoo.com>
2022-01-07 16:52:31 -0800

    added sendFile

--- myApp.js
+++ myApp.js
@@ -2,5 +2,6 @@ var express = require('express');
 var app = express();
 
 console.log('Hello World');
-app.get('/', (req, res) => res.send('Hello Express'));
+// app.get('/', (req, res) => res.send('Hello Express'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 module.exports = app;
